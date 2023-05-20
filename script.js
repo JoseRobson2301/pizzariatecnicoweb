a7f85535247b1d2eb855a38df17e07cf288300cb
Jose Robson <111148505+JoseRobson2301@users.noreply.github.com>
2023-05-20 17:22:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,58 +1,42 @@
+function toggleDisplay(element) {
+    if (element.style.display === 'none') {
+        element.style.display = 'block';
+    } else {
+        element.style.display = 'none';
+    }
+}
+
 var cardapioBtn = document.getElementById('cardapioBtn');
-        var navbar = document.querySelector('.navbar');
+var navbar = document.querySelector('.navbar');
 
-        cardapioBtn.addEventListener('click', function() {
-            if (navbar.style.display === 'none') {
-                navbar.style.display = 'block';
-            } else {
-                navbar.style.display = 'none';
-            }
-        });
+cardapioBtn.addEventListener('click', function() {
+    toggleDisplay(navbar);
+});
 
-    
-        
 var clickPizzaBtn = document.getElementById('clickPizzaBtn');
-    var opctionFood = document.querySelector('.opctionFood')
+var opctionFood = document.querySelector('.opctionFood');
 
-    clickPizzaBtn.addEventListener('click', function() {
-        if (opctionFood.style.display === 'none') {
-            opctionFood.style.display = 'block';
-        } else {
-            opctionFood.style.display = 'none';
-        }
-    });
+clickPizzaBtn.addEventListener('click', function() {
+    toggleDisplay(opctionFood);
+});
 
+var clickPastelBtn = document.getElementById('clickPastelBtn');
+var opctionFood02 = document.querySelector('.opctionFood02');
 
-    var clickPastelBtn = document.getElementById('clickPastelBtn');
-    var opctionFood02 = document.querySelector('.opctionFood02')
+clickPastelBtn.addEventListener('click', function() {
+    toggleDisplay(opctionFood02);
+});
 
-    clickPastelBtn.addEventListener('click', function() {
-        if (opctionFood02.style.display === 'none') {
-            opctionFood02.style.display = 'block';
-        } else {
-            opctionFood02.style.display = 'none';
-        }
-    });
+var clickCoxinhaBtn = document.getElementById('clickCoxinhaBtn');
+var opctionFood03 = document.querySelector('.opctionFood03');
 
+clickCoxinhaBtn.addEventListener('click', function() {
+    toggleDisplay(opctionFood03);
+});
 
-    var clickCoxinhaBtn = document.getElementById('clickCoxinhaBtn');
-    var opctionFood03 = document.querySelector('.opctionFood03')
+var clickEmpadaBtn = document.getElementById('clickEmpadaBtn');
+var opctionFood04 = document.querySelector('.opctionFood04');
 
-    clickCoxinhaBtn.addEventListener('click', function() {
-        if (opctionFood03.style.display === 'none') {
-            opctionFood03.style.display = 'block';
-        } else {
-            opctionFood03.style.display = 'none';
-        }
-    });
-
-    var clickEmpadaBtn = document.getElementById('clickEmpadaBtn');
-    var opctionFood04 = document.querySelector('.opctionFood04')
-
-    clickEmpadaBtn.addEventListener('click', function() {
-        if (opctionFood04.style.display === 'none') {
-            opctionFood04.style.display = 'block';
-        } else {
-            opctionFood04.style.display = 'none';
-        }
-    });
+clickEmpadaBtn.addEventListener('click', function() {
+    toggleDisplay(opctionFood04);
+});
