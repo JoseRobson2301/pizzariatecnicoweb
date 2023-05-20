9f34232ce3c02b7ef9692e340a0952a9843e8494
Jose Robson <111148505+JoseRobson2301@users.noreply.github.com>
2023-05-20 17:07:24 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -8,3 +8,51 @@ var cardapioBtn = document.getElementById('cardapioBtn');
                 navbar.style.display = 'none';
             }
         });
+
+    
+        
+var clickPizzaBtn = document.getElementById('clickPizzaBtn');
+    var opctionFood = document.querySelector('.opctionFood')
+
+    clickPizzaBtn.addEventListener('click', function() {
+        if (opctionFood.style.display === 'none') {
+            opctionFood.style.display = 'block';
+        } else {
+            opctionFood.style.display = 'none';
+        }
+    });
+
+
+    var clickPastelBtn = document.getElementById('clickPastelBtn');
+    var opctionFood02 = document.querySelector('.opctionFood02')
+
+    clickPastelBtn.addEventListener('click', function() {
+        if (opctionFood02.style.display === 'none') {
+            opctionFood02.style.display = 'block';
+        } else {
+            opctionFood02.style.display = 'none';
+        }
+    });
+
+
+    var clickCoxinhaBtn = document.getElementById('clickCoxinhaBtn');
+    var opctionFood03 = document.querySelector('.opctionFood03')
+
+    clickCoxinhaBtn.addEventListener('click', function() {
+        if (opctionFood03.style.display === 'none') {
+            opctionFood03.style.display = 'block';
+        } else {
+            opctionFood03.style.display = 'none';
+        }
+    });
+
+    var clickEmpadaBtn = document.getElementById('clickEmpadaBtn');
+    var opctionFood04 = document.querySelector('.opctionFood04')
+
+    clickEmpadaBtn.addEventListener('click', function() {
+        if (opctionFood04.style.display === 'none') {
+            opctionFood04.style.display = 'block';
+        } else {
+            opctionFood04.style.display = 'none';
+        }
+    });
